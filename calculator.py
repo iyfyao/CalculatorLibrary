d6ed2d3f5fc98a75ff57c6523fbcdc9e8a09d412
iyfyao <iyfsinan@gmail.com>
2023-03-05 22:22:44 +0100

    first commit calculator

---
+++ calculator.py
@@ -0,0 +1,9 @@
+"""
+Calculator module provides/performs basic math operation
+"""
+
+def addition(first_term, second_term):
+    return first_term + second_term
+
+def substractiont(first_term, second_term):
+    return first_term - second_term
