aca9d112550c6ce5b08f19054b585c8565f4e9db
iyfyao <iyfsinan@gmail.com>
2023-03-05 22:39:18 +0100

    Second commit with requirements

--- calculator.py
+++ calculator.py
@@ -2,8 +2,12 @@
 Calculator module provides/performs basic math operation
 """
 
+
 def addition(first_term, second_term):
+    """This function return the sum of two items"""
     return first_term + second_term
 
+
 def substractiont(first_term, second_term):
+    """This function return the difference of two items"""
     return first_term - second_term
